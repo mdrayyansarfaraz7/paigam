383bab7936a0d1708402810868bd21494f066793
Md Rayyan Sarfaraz <mdrayyansarfaraz@gmail.com>
2025-09-30 15:45:05 +0530

    index page updated

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import verificationEmailWithCodeV1 from "./templets/verifyCodeV1";
+import verificationEmailWithCodeV2 from "./templets/verifyCodeV2";
+
+import verificationEmailWithLinkV1 from "./templets/verifyLinkV1";
+import verificationEmailWithLinkV2 from "./templets/verifyLinkV2";
+
+import resetPasswordEmailV1 from "./templets/resetPasswordV1";
+import resetPasswordEmailV2 from "./templets/resetPasswordV2";
+
+import otpEmailV1 from "./templets/otpV1";
+import otpEmailV2 from "./templets/otpV2";
+
+import feedbackEmailV1 from "./templets/feedbackV1";
+import feedbackEmailV2 from "./templets/feedbackV2";
+
+import weMissYouEmail from "./templets/missYou";
+import specialOfferEmail from "./templets/marketing";
+import welcomeEmail from "./templets/welcome";
+
+import orderConfirmationEmail from "./templets/orderConfermation";
+import deliveryNotificationEmail from "./templets/delivery";
+import paymentReceiptEmail from "./templets/reciept";
+
+
+const paigam = {
+  verificationEmailWithCodeV1,
+  verificationEmailWithCodeV2,
+  verificationEmailWithLinkV1,
+  verificationEmailWithLinkV2,
+  resetPasswordEmailV1,
+  resetPasswordEmailV2,
+  otpEmailV1,
+  otpEmailV2,
+  feedbackEmailV1,
+  feedbackEmailV2,
+  weMissYouEmail,
+  specialOfferEmail,
+  welcomeEmail,
+  orderConfirmationEmail,
+  deliveryNotificationEmail,
+  paymentReceiptEmail
+};
+
+export default paigam;
